03a6c97bf6bb9dc1c156938cf4021a7f2120b5e8
Churchill Lee <hello@feliciousx.dev>
2020-03-15 16:59:15 +0800

    feat: initial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import { selectAll } from '/web_modules/d3.js'
+
+function kek () {
+  selectAll('#app')
+    .style('height', '100px')
+    .style('width', '100px')
+    .style('background-color', 'red')
+}
+
+kek()
